58d21f51914c2b10b25f4c96dd0574601ff1cb13
kalebtoroitich <kalebtoroitich@gmail.com>
2025-01-17 23:23:22 +0300

    submitting assigments

---
+++ python/week2.py
@@ -0,0 +1,17 @@
+my_list = []
+
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+
+my_list.insert(1, 15)
+
+my_list.extend([50,60,70])
+print(len(my_list))
+my_list.pop(7)
+
+my_list.sort()
+print(in
+print(my_list)
+
